e736b5bc3847ac01281cc04cf9e4f56d9bd1b672
Arthur Lin <arthur4456645@gmail.com>
2024-06-23 17:10:35 +0800

    finish JS logic v.1

---
+++ main.js
@@ -0,0 +1,132 @@
+// 金額輸入欄位
+const bill = document.querySelector("#bill");
+//人數輸入欄位
+const people = document.querySelector("#people");
+//客製化小費
+const customTip = document.querySelector(".customTip");
+//小費輸入欄位
+const inputBtn = document.querySelectorAll('input[type="button"]');
+//小費/人數 顯示欄位
+const tipPerPerson = document.querySelector(".tipPerPerson");
+//總金額/人數 顯示欄位
+const totalPerPerson = document.querySelector(".totalPerPerson");
+//輸入警告
+const peopleAlert = document.querySelector(".people-input span");
+const tipsAlert = document.querySelector(".tips span");
+//Reset button
+const resetBtn = document.querySelector(".resetBtn");
+
+//小費趴數
+let tip;
+
+//除人數後的小費
+function getEvenTip() {
+  if (!tip) return;
+  let tipAmount = Math.round((Number(bill.value) * tip) / Number(people.value));
+  tipPerPerson.innerText = `$ ${tipAmount}`;
+}
+
+//除人數後的總金額
+function getEvenTotal() {
+  if (!tip) return;
+  let total = Math.round(
+    (Number(bill.value) + Number(bill.value) * tip) / Number(people.value)
+  );
+  totalPerPerson.innerText = `$ ${total}`;
+}
+
+//移除按鈕的active狀態
+function removeActive() {
+  inputBtn.forEach((input) => {
+    if (input.classList.contains("active")) {
+      input.classList.remove("active");
+    }
+  });
+}
+
+//按按鈕後把tip的value放進變數中
+//並判斷金額與人數欄位是否有值，有的話就再次運算
+inputBtn.forEach((input) => {
+  input.addEventListener("click", () => {
+    tip = Number(input.value.replace("%", "")) / 100;
+    tipsAlert.style.display = "none";
+    removeActive();
+    if (bill.value && people.value) {
+      getEvenTip();
+      getEvenTotal();
+    }
+    input.classList.add("active");
+  });
+});
+
+//輸入人數後觸發計算函式
+people.addEventListener("input", () => {
+  validation();
+  if (people.value.trim().length === 0) {
+    tipsAlert.style.display = "block";
+    removeActive();
+    reset();
+    return;
+  } else {
+    getEvenTip();
+    getEvenTotal();
+  }
+});
+
+//綁定驗證
+bill.addEventListener("change", validation);
+
+//客製化小費
+customTip.addEventListener("click", () => {
+  let currentValue = customTip.value;
+  if (typeof currentValue === "string") {
+    customTip.value = "";
+  }
+  customTip.type = "number";
+});
+
+//客製化小費按鈕邏輯
+customTip.addEventListener("input", () => {
+  tip = customTip.value / 100;
+  if (bill.value && people.value) {
+    getEvenTip();
+    getEvenTotal();
+  }
+});
+
+//表單驗證
+function validation() {
+  //金額輸入框驗證
+  if (bill.value !== "") {
+    bill.style.border = "2px solid #449393";
+  } else {
+    bill.style.border = "0";
+  }
+  //人數輸入框驗證
+  if (!people.value) {
+    people.style.border = "2px solid #fa5252";
+    people.style.outline = "0";
+    peopleAlert.style.display = "block";
+  } else {
+    people.style.border = "0";
+    peopleAlert.style.display = "none";
+  }
+}
+
+//reset按鈕
+resetBtn.addEventListener("click", () => {
+  bill.value = "";
+  people.value = "";
+  customTip.value = "";
+  removeActive();
+  bill.focus();
+  reset();
+});
+
+function reset() {
+  tip = 0;
+  tipPerPerson.innerText = "$0";
+  totalPerPerson.innerText = "$0";
+  customTip.type = "button";
+  customTip.value = "Custom";
+}
